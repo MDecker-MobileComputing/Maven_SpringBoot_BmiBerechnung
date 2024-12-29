b3c49c5c8c434023a501bc07010f94992248457b
MDecker-MobileComputing <mide_42b@yahoo.de>
2024-12-29 19:58:27 +0100

    Projekt frisch mit Spring Initizalizr erzeugt https://start.spring.io/#!type=maven-project&language=java&platformVersion=3.4.1&packaging=jar&jvmVersion=23&groupId=de.eldecker.spring&artifactId=bmirechner&name=BMI-Rechner&description=Demo-Projekt%20f%C3%BCr%20Session-Bean&packageName=de.eldecker.spring.bmirechner&dependencies=web https://start.spring.io/#!type=maven-project&language=java&platformVersion=3.4.1&packaging=jar&jvmVersion=23&groupId=de.eldecker.spring&artifactId=bmirechner&name=BMI-Rechner&description=Demo-Projekt%20f%C3%BCr%20Session-Bean&packageName=de.eldecker.spring.bmirechner&dependencies=web

---
+++ src/main/java/de/eldecker/spring/bmirechner/BmiRechnerApplication.java
@@ -0,0 +1,13 @@
+package de.eldecker.spring.bmirechner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BmiRechnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BmiRechnerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/eldecker/spring/bmirechner/BmiRechnerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.eldecker.spring.bmirechner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BmiRechnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
